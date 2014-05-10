715059a6124e061270f598256e8603ea3605e511
Smirnov Alexey <chemistmail@gmail.com>
2014-05-11 00:36:04 +0400

    replace yaml

--- src/Process/Configurator.hs
+++ src/Process/Configurator.hs
@@ -1,20 +1,18 @@
 module Process.Configurator
-( store, readConfig )
+( store)
 where
 
-import           Types
-import           Process.Utils
 import           Process.Configurator.Yaml
+import           Process.Utils
+import           Types
 
 import           Control.Distributed.Process
 import           Control.Monad.State
-import System.Directory
-import Data.Time (UTCTime)
-import           Data.Map                     (lookup)
-import           Prelude                      hiding (lookup)
-
-readConfig :: IO (Either String (), Monitoring, Log)
-readConfig = runGns (StartOptions "gnc.yaml") emptyMonitoring parseConfig
+import           Data.Map                    (lookup)
+import           Data.Time                   (UTCTime)
+import           Data.Vector                 ((!?))
+import           Prelude                     hiding (lookup)
+import           System.Directory
 
 type StoreT a = StateT Monitoring Process a
 
@@ -22,7 +20,7 @@ store :: Process ()
 store = do
     say "start configurator"
     register "configurator" =<< getSelfPid
-    (_, m, _) <- liftIO readConfig
+    Right m <- liftIO $ parseConfig "gnc.yaml"
 --    say . show =<< liftIO getCurrentDirectory
     time <- liftIO $ getModificationTime "gnc.yaml"
     evalStateT (storeT time) m
@@ -35,7 +33,7 @@ storeT time = do
     newTime <- liftIO $ getModificationTime "gnc.yaml"
     when (time /= newTime) $ do
         lift $ say "file was changed, reload"
-        (_, n, _) <- liftIO readConfig
+        Right n <- liftIO $ parseConfig "gnc.yaml"
         put n
         lift $ nsend "cron" CronMap
         lift $ say "configurator (CronMap)-> cron  "
@@ -45,7 +43,7 @@ storeT time = do
         lift $ say "configurator (CheckMap)-> tasker  "
     storeT newTime
 
-storeMatch :: [Monitoring -> Match ()] 
+storeMatch :: [Monitoring -> Match ()]
 storeMatch = [ lookupTrigger
              , lookupCheck
              , lookupCronSet
@@ -57,48 +55,48 @@ lookupTrigger :: Monitoring -> Match ()
 lookupTrigger st = match fun
      where
      fun (Request (pid, TriggerId x)) = do
-         say "configurator <- (Request Trigger)"  
-         say "configurator (Response Trigger) -> "   
-         send pid $ Response (lookup (TriggerId x) (_triggers st))
+         say "configurator <- (Request Trigger)"
+         say "configurator (Response Trigger) -> "
+         send pid $ Response ((_triggers st) !? x)
 
 lookupCheck :: Monitoring -> Match ()
 lookupCheck st = match fun
      where
      fun (Request (pid, CheckId x)) = do
-         say "configurator <- (Request Check)"  
-         say "configurator (Response Check) -> "   
-         send pid $ Response (lookup (CheckId x) (_checks st))
+         say "configurator <- (Request Check)"
+         say "configurator (Response Check) -> "
+         send pid $ Response ((_checks st) !? x)
 
 lookupCronSet :: Monitoring -> Match ()
 lookupCronSet st = match fun
      where
      fun (Request (pid, Cron x)) = do
-         say "configurator <- (Request CronSet)"  
-         say "configurator (Response CronSet) -> "   
+         say "configurator <- (Request CronSet)"
+         say "configurator (Response CronSet) -> "
          send pid $ Response (lookup (Cron x) (_periodMap st))
 
 lookupCheckHost :: Monitoring -> Match ()
 lookupCheckHost st = match fun
      where
      fun (Request (pid, CheckHost x)) = do
-         say "configurator <- (Request CheckHosts)"  
-         say "configurator (Response CheckHosts) -> "   
+         say "configurator <- (Request CheckHosts)"
+         say "configurator (Response CheckHosts) -> "
          send pid $ Response (lookup (CheckHost x) (_checkHost st))
 
 getPart :: Monitoring -> Match ()
 getPart st = match fun
      where
      fun (Request (pid, CronMap)) = do
-         say "configurator <- (Request CronMap)"  
-         say "configurator (Response CronMap) -> "   
+         say "configurator <- (Request CronMap)"
+         say "configurator (Response CronMap) -> "
          send pid $ Response (Just $ _periodMap st)
      fun (Request (pid, HostMap)) = do
-         say "configurator <- (Request HostMap)"  
-         say "configurator (Response HostMap) -> "   
+         say "configurator <- (Request HostMap)"
+         say "configurator (Response HostMap) -> "
          send pid $ Response (Just $ _hosts st)
      fun (Request (pid, CheckMap)) = do
-         say "configurator <- (Request CheckMap)"  
-         say "configurator (Response CheckMap) -> "   
+         say "configurator <- (Request CheckMap)"
+         say "configurator (Response CheckMap) -> "
          send pid $ Response (Just $ _checks st)
      fun (Request (_, _)) = do
          say "configurator <- (BadMessage)"
--- src/Process/Configurator/Yaml.hs
+++ src/Process/Configurator/Yaml.hs
@@ -1,199 +1,209 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Process.Configurator.Yaml  where
+module Process.Configurator.Yaml
+(parseConfig) where
+
+import           Control.Monad            (mzero)
+import           Control.Applicative      (pure, (<$>), (<*>))
+import           Control.Arrow            ((&&&))
+
+import           Data.Attoparsec.Text     (parseOnly)
+import           Data.HashMap.Strict      (toList)
+import qualified Data.Map                 as M
+import           Data.Monoid              ((<>))
+import qualified Data.Set                 as S
+import           Data.Text                (Text)
+import           Data.Vector              (Vector, filter, findIndex, foldl,
+                                           foldl', foldl1, map, mapM, (!))
+import           Data.Yaml                (FromJSON (..), Value (..),
+                                           decodeFileEither, (.:), (.:?))
+import           System.Cron.Parser       (cronSchedule)
+
+import           Prelude                  hiding (filter, foldl, foldl1, map)
 
 import           Process.Configurator.Dsl (parseTrigger)
-import           Types
+import           Types                    (Check (..), CheckHost (..),
+                                           CheckId (..), CheckName (..),
+                                           Group (..), GroupName (..),
+                                           HostId (..), Hostname (..),
+                                           Monitoring (..), Status (..),
+                                           Trigger (..), TriggerHost (..),
+                                           TriggerId (..), TriggerName (..))
+import           Types.Cron               (Cron (..))
 
-import           Control.Applicative      ((<$>))
-import           Control.Arrow
-import           Control.Exception
-import           Control.Monad.Error
-import           Control.Monad.RWS
-import           Data.Attoparsec.Text     (parseOnly)
-import           Data.ByteString          (ByteString)
-import           Data.Either
-import           Data.Map                 (Map, fromList, toList, (!))
-import qualified Data.Map                 as Map
-import           Data.Set                 (Set)
-import qualified Data.Set                 as Set
-import           Data.Text                hiding (empty, filter, group, head,
-                                           map, zip)
-import           Data.Yaml.Syck
-import           Prelude                  hiding (not, or)
-import           System.Cron.Parser
+data ITrigger = ITrigger
+  { tname        :: Text
+  , tdescription :: Text
+  , tcheck       :: Text
+  , tresult      :: Text
+  } deriving Show
 
-inv :: Map CheckId Check -> Map CheckName CheckId
-inv x = fromList $ map (\(a,b) -> (_checkName b,a)) $ toList x
+data ICheck = ICheck
+  { iname   :: Text
+  , iperiod :: Text
+  , iparams :: [(Text, Value)]
+  } deriving Show
 
-inv' :: Map TriggerId Trigger -> Map TriggerName (TriggerId, Trigger)
-inv' x = fromList $ map (\(a,b) -> (_name b, (a,b))) $ toList x
+data IGroup = IGroup
+  { gname     :: Text
+  , ghosts    :: [Hostname]
+  , gtriggers :: Maybe [Text]
+  , gchecks   :: Maybe [Text]
+  } deriving Show
 
-inv'' :: Map HostId Hostname -> Map Hostname HostId
-inv'' x = fromList $ map (\(a,b) -> (b,a)) $ toList x
+data Config = Config
+  { chosts    :: Vector Hostname
+  , cgroups   :: Vector IGroup
+  , cchecks   :: Vector ICheck
+  , ctriggers :: Vector ITrigger
+  } deriving Show
 
-checkConf :: Monitoring -> Monitoring
-checkConf x = x { _checkHost = allMonit x
-                , _periodMap = allCron x (cronSet $ _checks x)
-                , _status = allTriggerHost x
-                }
-
-type IdChecks = Map CheckId Check
-type CronChecks = Map Cron (Set CheckHost)
-
-allCron :: Monitoring -> Set Cron -> CronChecks
-allCron mon s = Map.fromSet (fun mon) s
-  where checks' = _checks mon
-        fun :: Monitoring -> Cron -> Set CheckHost
-        fun m c = Set.filter (\(CheckHost (_,x)) -> c == _period (checks' ! x )) $ allCheckHost m
-
-cronSet :: IdChecks -> Set Cron
-cronSet x = Map.foldl fun Set.empty x
-  where fun :: Set Cron -> Check -> Set Cron
-        fun a b = Set.insert (_period b) a
-
-checkByGroup :: Monitoring -> Group -> (Set HostId, Set CheckId)
-checkByGroup m g = let fun :: Monitoring -> TriggerId -> CheckId
-                       fun mon t = _check $ _triggers mon Map.! t
-                       tc = Set.map (fun m) $ triggers g
-                       y = checks g `Set.union` tc
-                   in (hosts g, y)
+instance FromJSON Config where
+    parseJSON (Object v) = Config        <$>
+                           v .: "hosts"  <*>
+                           v .: "groups" <*>
+                           v .: "checks" <*>
+                           v .: "triggers"
+    parseJSON _ = mzero
 
 
-triggerHost :: Group -> Set TriggerHost
-triggerHost g = Set.fromList $ [ TriggerHost (a,b)
-                               | a <- Set.toList $ hosts g
-                               , b <- Set.toList $ triggers g
-                               ]
+instance FromJSON Hostname where
+    parseJSON (String x) = pure $ Hostname x
+    parseJSON _ = mzero
 
-allTriggerHost :: Monitoring -> Map TriggerHost Status
-allTriggerHost m = Map.fromSet (\_ -> Status True) $ Set.unions $ map triggerHost $ _groups m
+instance FromJSON ITrigger where
+    parseJSON (Object v) = ITrigger <$>
+                           v .: "name" <*>
+                           v .: "description" <*>
+                           v .: "check" <*> do
+                           v .: "result"
+    parseJSON _ = mzero
 
-checkHost :: (Set HostId, Set CheckId) -> Set CheckHost
-checkHost (x, y) = Set.fromList $ [ CheckHost (a,b)
-                                  | a <- Set.toList x
-                                  , b <- Set.toList y
-                                  ]
+instance FromJSON ICheck where
+    parseJSON (Object v) = do
+        n <-  v .: "name"
+        p <-  v .: "period"
+        return $ ICheck n p (toList v)
+    parseJSON _ = mzero
 
-allCheckHost :: Monitoring -> Set CheckHost
-allCheckHost m = let a = map (checkByGroup m) $ _groups m
-                 in Set.unions $ map checkHost a
+instance FromJSON IGroup where
+    parseJSON (Object v) = IGroup <$>
+                           v .: "name" <*>
+                           v .: "hosts" <*>
+                           v .:? "triggers" <*>
+                           v .:? "checks"
+    parseJSON _ = mzero
 
-allMonit :: Monitoring -> Map CheckHost (Set TriggerId)
-allMonit m  = let ach = allCheckHost m
-                  fun :: Monitoring -> CheckHost -> Set TriggerId
-                  fun m' (CheckHost (a, b)) = Set.unions $ map triggers $ filter (\x -> Set.member a (hosts x) && Set.member b (checks x) ) $ _groups m'
-              in Map.fromSet (fun m) ach
+decodeConf :: FilePath -> IO (Either String Config)
+decodeConf fp = conv <$> (decodeFileEither fp)
 
-parseConfig :: Gns ()
-parseConfig = do
-    s <- ask
-    st <- get
-    result <- liftIO $ (try $ do
-        EMap root <- n_elem <$> parseYamlFile (config s)
-        che <- mapM runErrorT $ unpackChecks root
-        let che' = fromList $ zip (map CheckId [1 .. ]) (rights che)
-        tr <- mapM runErrorT $ unpackTriggers root (inv che')
-        let tr' = fromList $ zip (map TriggerId [1 .. ]) (rights tr)
-            hs = unpackHosts root
-            gr = unpackGroups (inv che') tr' (inv'' hs) root
-        return $ st { _hosts = hs
-                    , _groups = gr
-                    , _triggers = tr'
-                    , _checks = che'
-                    }) :: Gns (Either SomeException Monitoring)
-    either (\e -> throwError $ "cant read config " ++ show e) fun result
-    where
-    fun :: Monitoring -> Gns ()
-    fun f = tell "success read and parse config\n" >> put (checkConf f)
+transformCheck :: ICheck -> Either String Check
+transformCheck ch = makeCheck =<< parseOnly cronSchedule (iperiod ch)
+  where
+    makeCheck cr = return $ Check
+      { _checkName = CheckName (iname ch)
+      , _period = Cron cr
+      , _params = M.filterWithKey (\x _ -> x /= "name" && x /= "period") $ M.map (\(String x) -> x) $ M.fromList $ iparams ch
+      }
 
------------------------------------------------------------------------------------------
--- helpers
------------------------------------------------------------------------------------------
+transformTrigger :: Vector Check -> ITrigger -> Either String Trigger
+transformTrigger chs tr = makeTrigger =<< (conv $ parseTrigger (tresult tr))
+  where
+    makeTrigger tr' = case (findIndex (\c -> _checkName c == CheckName (tcheck tr)) chs) of
+                           Nothing -> fail $ "check " ++ (show $ tcheck tr) ++ " not found"
+                           Just i -> return $ Trigger (TriggerName (tname tr)) (tdescription tr) (CheckId i) tr'
 
-unElem :: YamlNode -> Text
-unElem = unpackEStr . n_elem
+conv :: Show a => Either a b -> Either String b
+conv x = case x of
+              Left y -> Left $ show y
+              Right y -> Right y
 
-unNode :: [(YamlNode, YamlNode)] -> ByteString -> YamlNode
-unNode root node = case (filter (\(y,_) -> n_elem y == EStr node) root) of
-                       [(_, x)] -> x
-                       _ -> nilNode { n_elem = ESeq [] }
+transformGroup :: Vector Check -> Vector Hostname -> Vector Trigger -> IGroup -> Either String Group
+transformGroup ch hs tr gr = do
+                             let thosts :: Either String [HostId]
+                                 thosts = Prelude.mapM funh $ ghosts gr
+                                 funh :: Hostname -> Either String HostId
+                                 funh h = case (findIndex (== h) hs) of
+                                              Nothing -> fail $ "bad hostname in group"
+                                              Just i -> return $ HostId i
+                                 tchecks :: Maybe [Text] -> Either String [CheckId]
+                                 tchecks Nothing = return []
+                                 tchecks (Just xs) = Prelude.mapM func xs
+                                 func :: Text -> Either String CheckId
+                                 func h = case (findIndex (\a -> _checkName a == CheckName h) ch) of
+                                               Nothing -> fail "bad check in group"
+                                               Just i -> return $ CheckId i
+                                 ttriggers :: Maybe [Text] -> Either String [TriggerId]
+                                 ttriggers Nothing = return []
+                                 ttriggers (Just xs) = Prelude.mapM funt xs
+                                 funt :: Text -> Either String TriggerId
+                                 funt h = case (findIndex (\a -> _name a == TriggerName h) tr) of
+                                               Nothing -> fail "bad trigger in group"
+                                               Just i -> return $ TriggerId i
+                             hh <- thosts
+                             cc <- tchecks $ gchecks gr
+                             tt <- ttriggers $ gtriggers gr
+                             return $ Group (GroupName $ gname gr) (S.fromList hh) (S.fromList tt) (S.fromList cc)
 
-unEN :: [(YamlNode, YamlNode)] -> ByteString -> Text
-unEN x = unElem . unNode x
+--------------------------------------------------------------------------------------------------
+    -- _checkHosts
+--------------------------------------------------------------------------------------------------
+checksFromTriggers :: Vector Trigger -> Group -> (S.Set HostId, S.Set CheckId)
+checksFromTriggers t g =
+  let ch = S.map (\(TriggerId i) -> _check (t ! i)) $ triggers g
+  in (hosts g, ch)
 
------------------------------------------------------------------------------------------
--- hosts
------------------------------------------------------------------------------------------
+checkHosts :: Vector Trigger -> Vector Group -> S.Set CheckHost
+checkHosts t m =
+  let pairs = map (checksFromTriggers t) m
+      pairs' = map (hosts &&& checks) m
+      checkHost (x, y) = S.fromList $ [ CheckHost (a, b) | a <-  S.toList x, b <- S.toList y ]
+  in foldl1 S.union $ map checkHost (pairs <> pairs')
 
-unpackHosts :: [(YamlNode, YamlNode)] -> Map HostId Hostname
-unpackHosts root = fromList $ zip (map HostId [1 .. ]) $ map Hostname $ unpackESeq $ unNode root "hosts"
+triggersByCheckHost :: Vector Trigger -> Vector Group -> M.Map CheckHost (S.Set TriggerId)
+triggersByCheckHost tv gv =
+  let fun :: CheckHost -> Vector Group -> S.Set TriggerId
+      fun (CheckHost (a, b)) tv' = foldl S.union S.empty $ map triggers . filter (filterFun a b) $ tv'
+      filterFun :: HostId -> CheckId -> Group -> Bool
+      filterFun h c g = S.member h (hosts g)  && S.member c (S.map (\(TriggerId x) -> _check (tv ! x)) $ triggers g)
+  in M.fromSet (flip fun gv) $ checkHosts tv gv
 
-unpackESeq :: YamlNode -> [Text]
-unpackESeq x = let ESeq l = n_elem x
-               in map unElem l
+--------------------------------------------------------------------------------------------------
+    -- _periodMap
+--------------------------------------------------------------------------------------------------
 
-unpackEStr :: YamlElem -> Text
-unpackEStr (EStr z) = pack $ unpackBuf z
-unpackEStr _ = throw $ PError "error in unpackEStr, must be EStr"
+type PeriodMap = M.Map Cron (S.Set CheckHost)
 
------------------------------------------------------------------------------------------
--- groups
------------------------------------------------------------------------------------------
+cronChecks :: Vector Check -> Vector Trigger -> Vector Group -> PeriodMap
+cronChecks vc vt vg = M.fromSet fun cronSet
+  where checkHosts' = checkHosts vt vg
+        fun :: Cron -> S.Set CheckHost
+        fun c = S.filter (filterFun c) $ checkHosts'
+        filterFun :: Cron -> CheckHost -> Bool
+        filterFun c (CheckHost (_, CheckId x)) = c == _period (vc ! x)
+        cronSet :: S.Set Cron
+        cronSet = foldl (\sc c -> S.insert (_period c) sc) S.empty vc
 
-unpackGroups :: Map CheckName CheckId -> Map TriggerId Trigger -> Map Hostname HostId -> [(YamlNode, YamlNode)] -> [Group]
-unpackGroups cc tt hs root = let ESeq l = n_elem $ unNode root "groups"
-                             in map (unpackGroup cc tt hs) l
+--------------------------------------------------------------------------------------------------
+    -- _status
+--------------------------------------------------------------------------------------------------
 
-unpackGroup :: Map CheckName CheckId -> Map TriggerId Trigger -> Map Hostname HostId -> YamlNode -> Group
-unpackGroup cc tt hs group = let EMap ls = n_elem group
-                                 triggers'' = Set.fromList $ map ((\x -> fst $ (inv' tt) ! x) . TriggerName) $ unpackESeq $ unNode ls "triggers"
-                                 checks'' = Set.fromList $ map ((cc !) . CheckName) $ unpackESeq $ unNode ls "checks"
-                                 triggerCheck :: Set TriggerId -> Set CheckId
-                                 triggerCheck = Set.map (\x -> _check $ tt ! x)
-                             in Group
-                                  { name = GroupName $ unEN ls "name"
-                                  , hosts = Set.fromList $ map ((hs !) . Hostname) $ unpackESeq $ unNode ls "hosts"
-                                  , triggers = triggers''
-                                  , checks  = checks'' `Set.union` triggerCheck triggers''
-                                  }
+triggerHosts :: Vector Group -> M.Map TriggerHost Status
+triggerHosts vg =
+  let ths g = S.fromList $ [ TriggerHost (a, b) | a <- S.toList (hosts g), b <- S.toList (triggers g)]
+  in M.fromSet (const (Status True)) $ foldl' S.union S.empty $ map ths vg
 
------------------------------------------------------------------------------------------
--- triggers
------------------------------------------------------------------------------------------
 
-unpackTriggers :: Monad m => [(YamlNode, YamlNode)] -> Map CheckName CheckId -> [ErrorT PError m Trigger]
-unpackTriggers root mc = let ESeq l = n_elem $ unNode root "triggers"
-                         in map (unpackTrigger mc) l
+configToMonitoring :: Config -> Either String Monitoring
+configToMonitoring x = do
+    ch <-  Data.Vector.mapM transformCheck $ cchecks x
+    tr <-  Data.Vector.mapM (transformTrigger ch) $ ctriggers x
+    gg <- Data.Vector.mapM (transformGroup ch (chosts x) tr) $ cgroups x
+    let tbch = triggersByCheckHost tr gg
+    let crch = cronChecks ch tr gg
+    let ths = triggerHosts gg
+    return $ Monitoring crch tbch (chosts x) gg tr ch ths
 
-unpackTrigger :: Monad m => Map CheckName CheckId -> YamlNode -> ErrorT PError m Trigger
-unpackTrigger mc trigger = let EMap ls = n_elem trigger
-                               pp = parseTrigger $ unElem $ unNode ls "result"
-                           in case pp of
-                                Left e -> throw $ PError $ "cant parse trigger " ++ show e
-                                Right y -> return $ Trigger
-                                    { _name = TriggerName $ unEN ls "name"
-                                    , _check = mc ! (CheckName $ unEN ls "check")
-                                    , _description = unEN ls "description"
-                                    , _result = y
-                                    }
-
------------------------------------------------------------------------------------------
--- checks
------------------------------------------------------------------------------------------
-
-unpackChecks :: Monad m => [(YamlNode, YamlNode)] -> [ErrorT PError m Check]
-unpackChecks root = let ESeq l = n_elem $ unNode root "checks"
-                    in map unpackCheck  l
-
-unpackCheck :: Monad m => YamlNode -> ErrorT PError m Check
-unpackCheck check = let EMap ls = n_elem check
-                        checkName = unNode ls "name"
-                        period = parseOnly cronSchedule $  unEN ls "period"
-                    in case period of
-                            Left e -> throw $ PError $ "cant parse cron schedule " ++ show e
-                            Right r -> return $ Check
-                              { _checkName = CheckName $ unElem checkName
-                              , _period = Cron r
-                              , _params = fromList $ map (unElem *** unElem) $ filter (\(x, _) -> n_elem x /= EStr "name" || n_elem x /= EStr "period") ls
-                              }
-
+parseConfig :: FilePath -> IO (Either String Monitoring)
+parseConfig file = do
+    f <-  decodeConf file
+    return $ configToMonitoring =<< id f
