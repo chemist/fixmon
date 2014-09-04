634168a4b00506646181728e07323ea8ddbcdbd6
Alexey Smirnov <chemistmail@gmail.com>
2014-09-04 13:20:39 +0400

    unused tests

--- src/Test.hs
+++ src/Test.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE GADTs #-}
 module Main where
 
-import Types.DslTypes (TriggerRaw(..), Any(..))
+import Types.Dynamic (Exp(..), Dyn(..))
 import Process.Configurator.Dsl  (parseTrigger)
 
 import Test.Framework
@@ -23,48 +23,5 @@ mainWithOpts :: Int -> IO ()
 mainWithOpts x = defaultMainWithOpts tests $ mempty { ropt_test_options = Just (mempty { topt_maximum_generated_tests = Just x })}
 
 tests :: [Test]
-tests = [ testProperty "Binary" checkBinary
-        , testProperty "Parser" checkParse
-        ]
-
-checkBinary :: TriggerRaw Bool -> Bool
-checkBinary x = en x == x 
-
-checkParse :: TriggerRaw Bool -> Bool
-checkParse x = parseTrigger (ps x) == Right x
-
-instance Arbitrary (TriggerRaw Int) where
-    arbitrary = Int <$> (arbitrary :: Gen Int)
-
-allowed :: String 
-allowed = [ 'A' .. 'Z'] ++ [ 'a' .. 'z' ] ++ ['0' .. '9'] ++ "."
-
-instance Arbitrary (TriggerRaw Text) where
-        arbitrary = Text . fromString <$> (vectorOf 8 $ elements allowed)
-
-instance Arbitrary Any where
-    arbitrary = oneof [ Any <$> (Bool <$> (arbitrary :: Gen Bool))
-                      , Any <$> (arbitrary :: Gen (TriggerRaw Int))
-                      , Any <$> (arbitrary :: Gen (TriggerRaw Text))
-                      ]
-
-instance Arbitrary (TriggerRaw Bool) where
-    arbitrary = oneof [ mle
-                      , Not <$> mle
-                      , And <$> mle <*> mle
-                      , Or  <$> mle <*> mle
-                      ] 
-                where bool' = Bool <$> (arbitrary :: Gen Bool)
-                      any' = arbitrary :: Gen Any
-                      mle = oneof [ More <$> (arbitrary :: Gen (TriggerRaw Text)) <*> any' 
-                                  , Less <$> (arbitrary :: Gen (TriggerRaw Text)) <*> any' 
-                                  , Equal <$> (arbitrary :: Gen (TriggerRaw Text)) <*> any' 
-                                  , bool'
-                                  ]
-
-en :: Binary a => a -> a
-en = decode . encode
-
-ps :: Show a => a -> Text
-ps = pack . show
+tests = []
 
