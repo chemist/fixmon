3741b871cfac0842173450a490049c3c269530d1
Alexey Smirnov <chemistmail@gmail.com>
2014-10-27 18:52:22 +0400

    fix

--- src/Check/Snmp.hs
+++ src/Check/Snmp.hs
@@ -21,37 +21,25 @@ import Numeric
 data Snmp = SnmpInterfaces deriving Show
 
 instance Checkable Snmp where
-    describe SnmpInterfaces = [ ( "snmp", False, checkSnmp, "snmp settings")
-                              ]
+    describe SnmpInterfaces = []
     route SnmpInterfaces = singleton "network.interface" doSnmpInterface
     routeCheck SnmpInterfaces = routeCheck' SnmpInterfaces "network.interface"
     
-
-checkSnmp = undefined
 
 interfacesOid :: ByteString
 interfacesOid = "1.3.6.1.2.1.2.2.1"
 
 doSnmpInterface :: Check -> IO [Complex]
-doSnmpInterface (Check _ h _ _ conf p) = do
-    let Just s = encodeUtf8 . fromDyn <$> lookup "sequrityName" p 
-        Hostname host = h
+doSnmpInterface (Check _ h _ _ conf _) = do
+    let Hostname host = h
         Just c = conf
-    r <- bracket (client c)
+    r <- bracket (client (c { hostname = unpack host }))
                 close
-                (\snmp -> bulkwalk snmp [oidFromBS interfacesOid])
+                (flip bulkwalk [oidFromBS interfacesOid])
     return $ map complex $ convert r
     
 testSnmp :: Check
-testSnmp = Check (CheckName "test") (Hostname "salt") (Cron daily) "snmp" Nothing
-  [ (Counter "sequrityName", toDyn ("aes" :: Text))
-  , (Counter "version", toDyn (3 :: Int))
-  , (Counter "privType", toDyn ("AES" :: Text))
-  , (Counter "privAuth", toDyn ("AuthPriv" :: Text))
-  , (Counter "authType", toDyn ("SHA" :: Text))
-  , (Counter "authPass", toDyn ("helloallhello" :: Text))
-  , (Counter "privPass", toDyn ("helloallhello" :: Text))
-  ]
+testSnmp = Check (CheckName "test") (Hostname "salt") (Cron daily) "snmp" Nothing []
 
 convert :: Suite -> [Interface]
 convert (Suite xs) = map convS $ groupBy fun $ sortBy sortFun xs
@@ -118,11 +106,14 @@ phy = "\224\203NQ\198C"
 formatText :: Value -> Text
 formatText (String x) = decodeLatin1 x
 formatText _ = throw $ BadValue "toText"
+{-# INLINE formatText #-}
+
 
 formatMac :: Value -> Text
 formatMac (String "") = ""
 formatMac (String x) = foldl1 (\a b -> a <> ":" <> b) $ T.chunksOf 2 $ pack $ BS.foldl (flip showHex) "" $ BS.reverse x
 formatMac _ = throw $ BadValue "toMac"
+{-# INLINE formatMac #-}
 
 
 formatInt :: Value -> Int
@@ -130,12 +121,14 @@ formatInt (Integer x) = fromIntegral x
 formatInt (Gaude32 x) = fromIntegral x
 formatInt (Counter32 x) = fromIntegral x
 formatInt x = throw $ BadValue $ "toInt" <> (pack $ show x)
+{-# INLINE formatInt #-}
 
 formatAdminStatus :: Value -> Text
 formatAdminStatus (Integer 1) = "up"
 formatAdminStatus (Integer 2) = "down"
 formatAdminStatus (Integer 3) = "testing"
 formatAdminStatus _ = throw $ BadValue "toBool"
+{-# INLINE formatAdminStatus #-}
 
 formatOperStatus :: Value -> Text
 formatOperStatus (Integer 1) = "up"
@@ -146,16 +139,19 @@ formatOperStatus (Integer 5) = "dormant"
 formatOperStatus (Integer 6) = "notPresent"
 formatOperStatus (Integer 7) = "lowerLayerDown"
 formatOperStatus _ = throw $ BadValue "formatOperStatus"
+{-# INLINE formatOperStatus #-}
 
 formatTimeTicks :: Value -> Int
 formatTimeTicks (TimeTicks x) = fromIntegral x
 formatTimeTicks _ = throw $ BadValue "formatTimeTicks"
+{-# INLINE formatTimeTicks #-}
 
 
 convS :: [Coupla] -> Interface
 convS xs =
     let (a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:a15:a16:a17:a18:a19:a20:a21:_) = map value xs
     in Interface a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21
+{-# INLINE convS #-}
 
 
 
